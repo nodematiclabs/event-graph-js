2c90f56d89442e5896019c95610eb000a2e56041
orenaksakal <orenaksakal@gmail.com>
2021-04-04 17:44:41 +0200

    migrate demo data

--- demo/main.js
+++ demo/main.js
@@ -1,8 +1,39 @@
-const eventGraphJSON = {};
+const EventGraph = window.EventGraph.default;
+const EventGraphInstance = new EventGraph();
 
-let EventGraph = window.EventGraph.default;
-let EventGraphInstance = new EventGraph();
+const nodes = [
+  {
+    id: 1,
+    label: "RUN",
+    shape: "circle",
+    color: "#111",
+    fixed: true,
+    x: 60,
+    y: 10,
+  },
+  { id: 2, label: "ENTER", x: 160, y: 100, fixed: true },
+  { id: 3, label: "START", x: 160, y: 200, fixed: true },
+  { id: 4, label: "ResetQ", x: 130, y: 100, fixed: true },
+  { id: 5, label: "LEAVE", x: 0, y: 100, fixed: true },
+];
 
-console.log("EventGraphInstance", EventGraphInstance);
+const edges = [
+  { from: 1, to: 2, color: { color: "red" } },
+  { from: 1, to: 4 },
+  { from: 2, to: 2 },
+  { from: 2, to: 3 },
+  { from: 3, to: 5 },
+  { from: 4, to: 4 },
+  { from: 5, to: 3 },
+];
 
-EventGraphInstance.init('#root');
+EventGraphInstance.init("#root", nodes, edges, {
+  interaction: { dragNodes: true, dragView: false },
+  physics: false,
+  edges: {
+    smooth: false,
+    arrows: {
+      to: { enabled: true, scaleFactor: 1 },
+    },
+  },
+});
--- src/lib/eventgraph.js
+++ src/lib/eventgraph.js
@@ -2,42 +2,22 @@ import { DataSet, Network } from "vis-network/standalone/esm/vis-network";
 
 class EventGraph {
   constructor() {
-    console.log(new DataSet({}));
+    console.log(6);
   }
 
-  nodes = new DataSet([
-    { id: 1, label: "RUN", shape: "circle", color: "#FFFF00" },
-    { id: 2, label: "ENTER" },
-    { id: 3, label: "START" },
-    { id: 4, label: "ResetQ" },
-    { id: 5, label: "LEAVE" },
-  ]);
-  edges = new DataSet([
-    { from: 1, to: 2, color: { color: "red" } },
-    { from: 1, to: 4 },
-    { from: 2, to: 2 },
-    { from: 2, to: 3 },
-    { from: 3, to: 5 },
-    { from: 4, to: 4 },
-    { from: 5, to: 3 },
-  ]);
-
-  init = (selector) => {
-    console.log("init fired");
+  init = (selector, nodes, edges, options) => {
     const element = document.querySelector(selector);
-    const data = { nodes: this.nodes, edges: this.edges };
-    const network = new Network(element, data, {
-      interaction: { dragNodes: true, dragView: false },
-      physics: { enabled: false },
-      edges: {
-        smooth: false,
-        arrows: {
-          to: { enabled: true, scaleFactor: 1 },
-        },
-      },
-    });
+    const data = { nodes: new DataSet(nodes), edges: new DataSet(edges) };
+    const network = new Network(element, data, options);
+
     network.on("click", (params) => {
       console.log(params);
+    });
+
+    setTimeout(function () {
+      data.nodes.forEach((node) => {
+        data.nodes.update({ id: node.id, fixed: false });
+      });
     });
   };
 }
