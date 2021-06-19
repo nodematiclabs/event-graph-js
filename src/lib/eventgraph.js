095af94c62076e9782d99f997c0daedb2d0734da
orenaksakal <orenaksakal@gmail.com>
2021-06-19 15:50:37 +0200

    feat: add parameters to edges

--- src/lib/eventgraph.js
+++ src/lib/eventgraph.js
@@ -5,43 +5,45 @@ class EventGraph {
     return input.map((item) => {
       return {
         id: item.expression,
-        label: item.expression,
+        label: `${item.expression} ${
+          item.parameters ? `(${item.parameters.join(",")})` : ""
+        } ${item.routine?.yield ? `[${item.routine?.yield}]` : ""}`,
         shape: "circle",
         color: "#e6e6e6",
         borderWidth: "2",
-        widthConstraint: { maximum: 50 },
+        widthConstraint: { maximum: 75 },
         margin: 15,
         ...item,
       };
     });
   };
 
-  drawEdgeValues = (edges, ctx, network) => {
-    edges.forEach((node) => {
-      var edgePosition = network.getPositions([node.id]);
-      ctx.fillStyle = "black";
-      ctx.font = "14px helvetica";
+  // drawEdgeValues = (edges, ctx, network) => {
+  //   edges.forEach((node) => {
+  //     var edgePosition = network.getPositions([node.id]);
+  //     ctx.fillStyle = "black";
+  //     ctx.font = "14px helvetica";
 
-      // node?.routine?.state_transition?.phase &&
-      //   ctx.fillText(
-      //     `phase = ${node.routine.state_transition.phase}`,
-      //     nodePosition[node.id].x - 60,
-      //     nodePosition[node.id].y + 60
-      //   );
-      // node?.routine?.state_transition?.queue &&
-      //   ctx.fillText(
-      //     `queue = ${node.routine.state_transition.queue}`,
-      //     nodePosition[node.id].x - 60,
-      //     nodePosition[node.id].y + 77
-      //   );
-      // node?.routine?.state_transition?.sigma &&
-      //   ctx.fillText(
-      //     `sigma = ${node.routine.state_transition.sigma}`,
-      //     nodePosition[node.id].x - 60,
-      //     nodePosition[node.id].y + 94
-      //   );
-    });
-  };
+  //     // node?.routine?.state_transition?.phase &&
+  //     //   ctx.fillText(
+  //     //     `phase = ${node.routine.state_transition.phase}`,
+  //     //     nodePosition[node.id].x - 60,
+  //     //     nodePosition[node.id].y + 60
+  //     //   );
+  //     // node?.routine?.state_transition?.queue &&
+  //     //   ctx.fillText(
+  //     //     `queue = ${node.routine.state_transition.queue}`,
+  //     //     nodePosition[node.id].x - 60,
+  //     //     nodePosition[node.id].y + 77
+  //     //   );
+  //     // node?.routine?.state_transition?.sigma &&
+  //     //   ctx.fillText(
+  //     //     `sigma = ${node.routine.state_transition.sigma}`,
+  //     //     nodePosition[node.id].x - 60,
+  //     //     nodePosition[node.id].y + 94
+  //     //   );
+  //   });
+  // };
 
   drawStateTransitions = (nodes, ctx, network) => {
     nodes.forEach((node) => {
@@ -53,19 +55,21 @@ class EventGraph {
         ctx.fillText(
           `phase = ${node.routine.state_transition.phase}`,
           nodePosition[node.id].x - 60,
-          nodePosition[node.id].y + 60
+          nodePosition[node.id].y + 65
         );
-      node?.routine?.state_transition?.queue &&
-        ctx.fillText(
-          `queue = ${node.routine.state_transition.queue}`,
-          nodePosition[node.id].x - 60,
-          nodePosition[node.id].y + 77
-        );
+
       node?.routine?.state_transition?.sigma &&
         ctx.fillText(
           `sigma = ${node.routine.state_transition.sigma}`,
           nodePosition[node.id].x - 60,
-          nodePosition[node.id].y + 94
+          nodePosition[node.id].y + 82
+        );
+
+      node?.routine?.state_transition?.queue &&
+        ctx.fillText(
+          `queue = ${node.routine.state_transition.queue}`,
+          nodePosition[node.id].x - 60,
+          nodePosition[node.id].y + 99
         );
     });
   };
@@ -111,6 +115,7 @@ class EventGraph {
   };
 
   init = (selector, input, options) => {
+    console.log("input", input);
     const nodes = this.gatherNodes(input);
     const edges = this.gatherEdges(input, nodes);
     const element = document.querySelector(selector);
@@ -119,7 +124,7 @@ class EventGraph {
 
     network.on("beforeDrawing", (ctx) => {
       this.drawStateTransitions(nodes, ctx, network);
-      this.drawEdgeValues(edges, ctx, network);
+      // this.drawEdgeValues(edges, ctx, network);
     });
 
     setTimeout(function () {
